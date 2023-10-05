46885dc99d6837c8d05350e4909c9948adf56f8c
Bilal Shakeel <info.bilalshakeel@gmail.com>
2023-10-06 00:34:33 +0500

    sequence routes added

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const homeController = require('../controllers/home/homeController');
 const aboutController = require('../controllers/about/aboutController');
 const stopController = require('../controllers/stops/stopController');
+const sequenceController = require('../controllers/sequence/sequenceController');
 
 // Home Page
 router.get('/', homeController.homepage);
@@ -17,5 +18,13 @@ router.get('/viewstop/:id', stopController.viewStop);
 router.get('/editstop/:id', stopController.editStop);
 router.put('/editstop/:id', stopController.editPost); 
 router.delete('/editstop/:id', stopController.deleteStop);
+
+// Sequence
+router.get('/addsequence', sequenceController.addSequence);
+router.post('/addsequence', sequenceController.postSequence);
+router.get('/viewsequence/:id', sequenceController.viewSequence);
+router.get('/editsequence/:id', sequenceController.editSequence);
+router.put('/editsequence/:id', sequenceController.editPost); 
+router.delete('/editsequence/:id', sequenceController.deleteSequence);
 
 module.exports = router;
