740771470658e194a1edfe1db22bd2ad0428a140
Bilal Shakeel <info.bilalshakeel@gmail.com>
2023-10-20 23:16:06 +0500

    calendar schema updated

--- server/models/calendar/Calendar.js
+++ server/models/calendar/Calendar.js
@@ -2,15 +2,41 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 const calendarSchema = new Schema({
- serviceId: { 
-    type: String, 
-    required: true, 
-    unique: true 
-},
-day: { 
-    type: String, 
-    required: true 
-},
+monday:[{
+    tripId: { 
+    type: Number, 
+    },
+  }],
+tuesday: [{
+    tripId: { 
+    type: Number, 
+    },
+  }],
+wednesday: [{
+    tripId: { 
+    type: Number, 
+    },
+  }],
+thrusday: [{
+    tripId: { 
+    type: Number, 
+    },
+  }],
+friday:[{
+    tripId: { 
+    type: Number, 
+    },
+  }],
+saturday: [{
+    tripId: { 
+    type: Number, 
+    },
+  }],
+sunday:[{
+    tripId: { 
+    type: Number, 
+    },
+  }],
 createdAt:{
     type: Date,
     default: Date.now()
