9b7307dd1c506db20dd25ed1f8877f1dccb0fde5
Bilal Shakeel <info.bilalshakeel@gmail.com>
2023-10-23 11:12:08 +0500

    calendar added

--- server/controllers/home/homeController.js
+++ server/controllers/home/homeController.js
@@ -2,6 +2,7 @@ const Stop = require('../../models/stop/Stop');
 const Sequence=require('../../models/sequence/Sequence');
 const Route = require('../../models/route/Route');
 const Trip = require('../../models/trip/Trip');
+const Calendar = require('../../models/calendar/Calendar');
 const mongoose = require('mongoose');
 
 exports.homepage = async (req, res) => {
@@ -21,6 +22,7 @@ exports.homepage = async (req, res) => {
        const sequences = await Sequence.find({}).limit(4);
        const routes = await Route.find({}).limit(4);
        const trips = await Trip.find({}).limit(4);
+       const calendars = await Calendar.find({}).limit(4);
 
       res.render('index', {
         locals,
@@ -28,6 +30,7 @@ exports.homepage = async (req, res) => {
         sequences,
         routes,
         trips,
+        calendars,
         messages
       });
 
